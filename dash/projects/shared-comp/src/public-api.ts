27d82212f03c745ab35cea64ed29add88dfced45
Nutty Geek <nuttygeeek@gmail.com>
2024-06-15 11:27:16 +0530

    reset

--- dash/projects/shared-comp/src/public-api.ts
+++
@@ -1,8 +0,0 @@
-/*
- * Public API Surface of shared-comp
- */
-
-export * from './lib/shared-comp.service';
-export * from './lib/shared-comp.component';
-export * from './lib/shared-comp.module';
-export * from './lib/material.module';
